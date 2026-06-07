24ab9bbbdfd65f36816797e2d562217ff220ed23
Tole <manutol@gmail.com>
2026-06-07 20:30:51 +0200

    chore(users): remove unused List import in InMemoryUserRepository

--- src/main/java/com/mtole/taskmanager/users/InMemoryUserRepository.java
+++ src/main/java/com/mtole/taskmanager/users/InMemoryUserRepository.java
@@ -3,7 +3,6 @@ package com.mtole.taskmanager.users;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
